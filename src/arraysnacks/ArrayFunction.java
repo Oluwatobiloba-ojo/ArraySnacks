dc4f10c91097b8d9d4db2aaae8b7adead9fe9926
Ojo Oluwatobi <ojot630@gmail.com>
2023-09-30 20:27:29 +0100

    final commit

--- src/arraysnacks/ArrayFunction.java
+++ src/arraysnacks/ArrayFunction.java
@@ -1,49 +1,71 @@
 package arraysnacks;
 
+import java.util.Arrays;
+
 public class ArrayFunction {
-    public static int largestElement(int[] arrays){
+    public static void main(String[] args) {
+        int[] array = {1, 3, 4, 6, 7};
+        String[] characters = {"a", "b", "c"};
+        String[] result = concatenateListWithList(array, characters);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int largestElement(int[] arrays) {
         int largest = arrays[0];
-        for(int integers = 0; integers < arrays.length; integers++){
-            if (arrays[integers] > largest)largest = arrays[integers];
+        for (int array : arrays) {
+            if (array > largest) largest = array;
         }
         return largest;
     }
-    public static String reverseList(int[] arrays){
-        String result = "";
-        for (int number = arrays.length - 1; number >= 0; number--){
-            result = result + " " +arrays[number];
+
+    public static int[] reverseList(int[] arrays) {
+        int [] result = new int[arrays.length];
+        int count = 0;
+        for (int number = arrays.length - 1; number >= 0; number--) {
+            result[count] = arrays[number];
+            count++;
         }
         return result;
     }
 
     public static boolean checkListElement(int[] integers, int element) {
         boolean result = false;
-        for (int number = 0; number < integers.length; number++){
-            if (element == integers[number])result = true;
+        for (int integer : integers) {
+            if (element == integer) {
+                result = true;
+                break;
+            }
         }
         return result;
     }
 
-    public static String oddPosition(int[] integers) {
-        String result= "";
-        for (int number = 1; number < integers.length; number+=2){
-            result = result + " " + integers[number];
+    public static int[] oddPosition(int[] integers) {
+        int [] odd = new int[integers.length/2];
+        int count = 0;
+        for (int number = 1; number < integers.length; number += 2) {
+            odd[count] =  integers[number];
+            count++;
         }
-        return result;
+        return odd;
     }
 
-    public static String evenPosition(int[] integers) {
-        String result = "";
-        for (int index = 0; index < integers.length; index+=2){
-            result = result + " "+ integers[index];
+    public static int[] evenPosition(int[] integers) {
+        int value;
+        if (integers.length % 2 == 0) value = integers.length/2;
+        else  value = integers.length/2 + 1;
+        int [] even = new int[value];
+        int counter = 0;
+        for (int index = 0; index < integers.length; index += 2) {
+            even[counter] = integers[index];
+            counter++;
         }
-        return result;
+        return even;
     }
 
     public static int totalArray(int[] array) {
         int total = 0;
-        for (int index = 0; index < array.length; index++) {
-            total += array[index];
+        for (int i : array) {
+            total += i;
         }
         return total;
     }
@@ -58,8 +80,8 @@ public class ArrayFunction {
 
     public static int forLoopSum(int[] integers) {
         int total = 0;
-        for (int number = 0; number < integers.length; number++){
-            total += integers[number];
+        for (int integer : integers) {
+            total += integer;
         }
         return total;
     }
@@ -67,7 +89,7 @@ public class ArrayFunction {
     public static int whileLoopSum(int[] integers) {
         int total = 0;
         int number = 0;
-        while (number < integers.length){
+        while (number < integers.length) {
             total += integers[number];
             number++;
         }
@@ -82,7 +104,51 @@ public class ArrayFunction {
             if (counter > 0) number++;
             total += integers[number];
             counter++;
-        }while (number < integers.length-1);
+        } while (number < integers.length - 1);
         return total;
+    }
+
+    public static String[] concatenateListWithList(int[] integers, String[] character) {
+        String[] array = new String[integers.length + character.length];
+        for (int i = 0; i < integers.length; i++) {
+            for (int a = 0; a <= i; a++) {
+                array[i] = String.valueOf(integers[a]);
+            }
+        }
+        int i = integers.length;
+        for (String s : character) {
+            array[i] = s;
+            if (i < array.length - 1) i++;
+        }
+        return array;
+    }
+
+    public static String[] combinesList(int[] integers, String[] chars) {
+        String[] array = new String[integers.length + chars.length];
+        int first = 0;
+        for (String aChar : chars) {
+            array[first] = aChar;
+            first += 2;
+        }
+        int second = 1;
+        for (int integer : integers) {
+            array[second] = String.valueOf(integer);
+            if (second < array.length - 2) second += 2;
+            else second++;
+        }
+        return array;
+    }
+
+    public static int[] convertIntegerToList(int number) {
+        String num = String.valueOf(number);
+        int[] array = new int[num.length()];
+        int count = num.length() - 1;
+        while (number != 0) {
+            int n = number % 10;
+            array[count] = n;
+            count--;
+            number /= 10;
+        }
+        return array;
     }
 }
